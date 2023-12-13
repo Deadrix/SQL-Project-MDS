304b571b1d33de794c112d70190f26f08f088909
DeadrixMDS <alexandre.bouriche@my-digital_school.org>
2023-12-13 23:56:25 +0100

    gestion des erreur de contrainte d'integrité pour les users qui dupliquent la cle primaire par leur correspondance de nom et de prenom

--- RUN_BEFORE_ANYTHING_ELSE/LinkUserAndAbonne.php
+++ RUN_BEFORE_ANYTHING_ELSE/LinkUserAndAbonne.php
@@ -16,15 +16,26 @@ $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $abonnes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$offset = 0;
 // For each abonne, we create a utilisateur
 foreach ($abonnes as $abonne) {
-    $sql = "INSERT INTO utilisateur (email, password, role, id_abonne) VALUES (:email, :password, 'abonne', :id_abonne)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindValue(":email", str_replace([" ", "'"], "", (strtolower($abonne['prenom'] . $abonne['nom']))) . "@sqlmds.fr");
-    $stmt->bindValue(":password", password_hash(str_replace([" ", "'"], "", (strtolower($abonne['prenom'] . $abonne['nom']))), PASSWORD_BCRYPT));
-    $stmt->bindValue(":id_abonne", $abonne["id"]);
-    $stmt->execute();
-
+    try {
+        $sql = "INSERT INTO utilisateur (email, password, role, id_abonne) VALUES (:email, :password, 'abonne', :id_abonne)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(":email", str_replace([" ", "'"], "", (strtolower($abonne['prenom'] . $abonne['nom']))) . "@sqlmds.fr");
+        $stmt->bindValue(":password", password_hash(str_replace([" ", "'"], "", (strtolower($abonne['prenom'] . $abonne['nom']))), PASSWORD_BCRYPT));
+        $stmt->bindValue(":id_abonne", $abonne["id"]);
+        $stmt->execute();
+    } catch (PDOException $e) {
+        echo $e->getMessage() . "\n";
+        $sql = "INSERT INTO utilisateur (email, password, role, id_abonne) VALUES (:email, :password, 'abonne', :id_abonne)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(":email", str_replace([" ", "'"], "", (strtolower($abonne['prenom'] . $abonne['nom']))) . $offset . "@sqlmds.fr");
+        $stmt->bindValue(":password", password_hash(str_replace([" ", "'"], "", (strtolower($abonne['prenom'] . $abonne['nom']) . $offset)), PASSWORD_BCRYPT));
+        $stmt->bindValue(":id_abonne", $abonne["id"]);
+        $stmt->execute();
+        $offset++;
+    }
 }
 
 $pdo = null;
