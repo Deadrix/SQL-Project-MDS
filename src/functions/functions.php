5c265574c32b6b3e20071ba482659f5eff9f8034
DeadrixMDS <alexandre.bouriche@my-digital-school.org>
2024-01-09 11:06:21 +0100

    PHPUnit

--- src/functions/functions.php
+++ src/functions/functions.php
@@ -4,9 +4,8 @@ require_once __DIR__ . "/../../config.php";
 
 function connectToDatabase(): PDO
 {
-    global $dsn, $user, $pass;
     try {
-        $pdo = new PDO($dsn, $user, $pass);
+        $pdo = new PDO(DSN, DB_USER, DB_PASS);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     } catch (PDOException $e) {
--- src/tests/functionsTest.php
+++ src/tests/functionsTest.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
+require_once __DIR__ . "/../../src/functions/functions.php";
+
 final class functionsTest extends TestCase
 {
     public function testConnectToDatabase(): void
@@ -11,5 +13,13 @@ final class functionsTest extends TestCase
     public function testGetBooks(): void
     {
         $this->assertIsArray(getBooks());
+        $this->assertIsArray(getBooks("12", "lapo", "flamma", "1"));
+
+        $this->assertCount(20, getBooks());
+        $this->assertCount(0, getBooks(titre: "loup", disponible: "0"));
+        $this->assertCount(1, getBooks("12", "lapo", "flamma", "1"));
+        $this->assertCount(2, getBooks(titre: "loup"));
+
+        $this->assertStringContainsString("12", getBooks()[0]["titre"]);
     }
 }
